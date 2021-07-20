5f25b91d5d85d3742ed8e3217b72e27a15c15c29
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2021-07-20 20:27:07 +0200

    FEAT styles and detail page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,7 +6,6 @@ use App\Entity\Serie;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
